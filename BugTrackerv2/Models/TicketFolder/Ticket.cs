c3f35c5e9c64cd9e81a26bd20cea77e502c09397
qizzle <theqizzler@gmail.com>
2015-04-28 17:12:05 -0400

    Initializing Ticket Model and Associative Models

--- BugTrackerv2/Models/TicketFolder/Ticket.cs
+++ BugTrackerv2/Models/TicketFolder/Ticket.cs
@@ -2,10 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using BugTrackerv2.Models.ProjectFolder;
 namespace BugTrackerv2.Models.TicketFolder
 {
     public class Ticket
     {
+        //data properties
+        public int TicketId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+        public string OwnerUserId { get; set; }
+        public string AssignedToUserId { get; set; }
+
+
+        //FKs
+        public int ProjectId { get; set; }
+        public int TicketTypeId { get; set; }
+        public int TicketPriorityId { get; set; }
+        public int TicketStatusId { get; set; }
+        
+
+
+        //nav propertiers
+        public Project project { get; set; }                    //1 project => many tickets
     }
 }
